0669952fb9918bc312c4f0f418d0e479a2f8c8a0
xTonyYu <xtonyyu@gmail.com>
2020-09-08 17:06:39 -0700

    form data and fetch

--- app.js
+++ app.js
@@ -1,4 +1,18 @@
 console.log('voter info 2020')
 
-const search = document.getElementById("search-address")
+// const search = document.getElementById('voter')
+const voterInfo = document.getElementById('voter-info')
+let address = ''
+voterInfo.addEventListener('submit', e => {
+  e.preventDefault();
+  console.log(e)
+  voterInfo.querySelectorAll('input').forEach( ele => {
+    console.dir(ele.value)
+    address += `${ele.value} `
+  })
+  console.log(address)
+  const URL = 'https://www.googleapis.com/civicinfo/v2/voterinfo?key=AIzaSyAHOsjZyYzVuSdwGFoQzKckUx7ygGZXjzQ&electionId=2000&address=' + address
+  fetch(URL).then(res => res.json())
+  .then(data => console.log(data))
+})
 
